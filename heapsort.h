c22807e1b5bf70351a88112bd0e0aae2ccc25b27
siryus1713 <76882969+siryus1713@users.noreply.github.com>
2021-05-10 17:40:38 +0800

    Create heapsort.h

---
+++ heapsort.h
@@ -0,0 +1,21 @@
+#define max 1000000
+
+int a[max];
+char str[max][102];
+
+    void swap(int *a, int *b);
+
+    void maxHeapify(int *array, int root, int length);
+
+    void BuildmaxHeap(int *arr);
+   
+    void HeapSort(int *array);
+
+    void str_swap(char *str1_ptr, char *str2_ptr);
+
+    void maxHeapify(char str[max][102], int root, int length);
+   
+    void BuildmaxHeap(char str[max][102]);
+    
+    void str_heapsort(char str[max][102]);
+   
