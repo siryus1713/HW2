b84e8eaa18b3b1e7dcf835f679ba46a221d13a74
siryus1713 <76882969+siryus1713@users.noreply.github.com>
2021-05-10 17:32:45 +0800

    Create qsort.h

---
+++ qsort.h
@@ -0,0 +1,17 @@
+#define max 1000000
+
+void int_swap(int *a, int *b);
+  
+void int_sort();
+
+int int_partition(int arr[], int low, int high);
+
+void int_quicksort(int arr[], int low, int high);
+
+void str_sort();
+
+void str_swap(char *str1_ptr, char *str2_ptr);
+   
+int str_partition(char str[max][102], int low, int high);
+   
+void str_quicksort(char arr[max][102], int low, int high);
